64231446b23230ad4e7dd32087328226fc0c36a2
tabitha-codes <tabithacodes@gmail.com>
2026-06-12 23:29:29 -0500

    finished Extension2

---
+++ LabExercises/exercise3.js
@@ -0,0 +1,11 @@
+
+
+function changeColumn1() {
+document.getElementById("column1").style.backgroundColor = 'blue';
+document.getElementById("heading1").textContent=document.getElementById("input1").value;
+}
+
+function changeColumn2() {
+document.getElementById("column2").style.backgroundColor = 'purple';
+document.getElementById("heading2").textContent=document.getElementById("input2").value;
+}
