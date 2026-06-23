5667f0a1e52b82ee8e6ffb51b730789fb35c06ce
tabitha-codes <tabithacodes@gmail.com>
2026-06-22 22:39:08 -0500

    Fix embedded git repo issue

---
+++ Module01/ClassExercise/helloworld.js
@@ -0,0 +1,15 @@
+// Slide 79 Mod 01
+// console.log('Hello World')
+
+// this is the structure of a function
+// function ThisIsTheName(Signature) {
+// Body
+// }
+function sayHello() {
+console.log('Hello')
+}
+
+function sum(a, b) {
+console.log(a + b)
+}
+sum(5, 1)
---
+++ Module01/LabExercises/exercise4.js
@@ -0,0 +1,36 @@
+// Addition
+function sum (a,b) {
+    console.log(a + b)
+}
+
+sum(5,1)
+
+// Subtract
+function subtract (a,b) {
+    console.log(a - b)
+}
+
+subtract(5,1)
+
+// Multiply
+function multiply (a,b) {
+    console.log(a * b)
+}
+
+multiply(5,5)
+
+// Divide
+function divide (a,b) {
+    console.log(a / b)
+}
+
+divide(40,5)
+
+function greet (name){
+    console.log("Hello " + name);
+}
+
+greet("Tabitha")
+
+// Output: HelloTabitha - because I didn't have a space. "Hello"
+// Output: Hello Tabitha
