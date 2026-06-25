74f57a84b94505106e38b147039259c4976717d0
tabitha-codes <tabithacodes@gmail.com>
2026-06-24 20:32:31 -0500

    exercise7

---
+++ Module01/LabExercises/exercise7.js
@@ -0,0 +1,35 @@
+// Addition
+function sum (a,b) {
+    console.log(a + b)
+}
+
+sum(10,1) // Unit 01: 11
+sum(10,10) // Unit 02: 20
+sum(10,20) // Unit 03: 30
+
+// Subtract
+function subtract (a,b) {
+    console.log(a - b)
+}
+
+subtract(10,1)  // Unit 01: 9
+subtract(10,10)  // Unit 02: 0
+subtract(20,10)  // Unit 03: 10
+
+// Multiply
+function multiply (a,b) {
+    console.log(a * b)
+}
+
+multiply(10,10)  // Unit 01: 100
+multiply(10,20)  // Unit 02: 200
+multiply(10,30)  // Unit 03: 300
+
+// Divide
+function divide (a,b) {
+    console.log(a / b)
+}
+
+divide(10,50)  // Unit 01: 0.2
+divide(10,60)  // Unit 02: 0.16666666666666666
+divide(10,70)  // Unit 03: 0.14285714285714285
