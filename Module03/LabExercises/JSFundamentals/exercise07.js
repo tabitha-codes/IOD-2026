0fb9350864ce1779e4e3653c1cd97bc7521c6c99
tabitha-codes <tabithacodes@gmail.com>
2026-07-14 18:32:01 -0500

    mod03ex07

--- Module03/LabExercises/JSFundamentals/exercise07.js
+++ Module03/LabExercises/JSFundamentals/exercise07.js
@@ -40,16 +40,234 @@ basketballGame
 
 /* 
 My Answer:
-Output:
+========================================
+a) 
+const basketballGame = {
+  score: 0,
+  freeThrow() {
+    this.score++;
+    return this;
+  },
+  basket() {
+    this.score += 2;
+    return this;
+  },
+  threePointer() {
+    this.score += 3;
+    return this;
+  },
+  halfTime() {
+    console.log("Halftime score is " + this.score);
+    return this;
+  },
+};
 
-Notes:
+basketballGame
+  .basket()
+  .freeThrow()
+  .freeThrow()
+  .basket()
+  .threePointer()
+  .halfTime();
 
-a)
+OUTPUT: Halftime score is 9
 
+========================================
 b) 
+const basketballGame = {
+  score: 0,
+  freeThrow() {
+    this.score++;
+    return this;
+  },
+  basket() {
+    this.score += 2;
+    return this;
+  },
+  threePointer() {
+    this.score += 3;
+    return this;
+  },
+  halfTime() {
+    console.log("Halftime score is " + this.score);
+    return this;
+  },
 
+  fullTime() {
+    console.log("Fulltime score is " + this.score);
+    return this;
+  }
+};
+
+basketballGame
+  .basket()
+  .freeThrow()
+  .freeThrow()
+  .basket()
+  .threePointer()
+  .halfTime()
+  .fullTime();
+
+OUTPUT: Halftime score is 9
+Fulltime score is 9
+========================================
 c)
 
-d)
+const basketballGame = {
+  score: 0,
+  fouls: 0,
 
+  freeThrow() {
+    this.score++;
+    return this;
+  },
+
+  foul() {
+    this.fouls++;
+    return this;
+  },
+
+  basket() {
+    this.score += 2;
+    return this;
+  },
+  threePointer() {
+    this.score += 3;
+    return this;
+  },
+  halfTime() {
+    console.log("Halftime score is " + this.score);
+    console.log("Foul count is " + this.fouls);
+    return this;
+  },
+
+  fullTime() {
+    console.log("Fulltime score is " + this.score);
+    console.log("Foul count is " + this.fouls);
+    return this;
+  },
+};
+
+basketballGame
+  .basket()
+  .freeThrow()
+  .freeThrow()
+  .basket()
+  .threePointer()
+  .foul()
+  .halfTime()
+  .fullTime();
+
+OUTPUT:
+Halftime score is 9
+Foul count is 1
+Fulltime score is 9
+Foul count is 1
+
+========================================
+d)
+const basketballGame = {
+  score: 0,
+  fouls: 0,
+
+  freeThrow() {
+    this.score++;
+    return this;
+  },
+
+  foul() {
+    this.fouls++;
+    return this;
+  },
+
+  basket() {
+    this.score += 2;
+    return this;
+  },
+  threePointer() {
+    this.score += 3;
+    return this;
+  },
+
+  halfTime() {
+    console.log("Halftime score is " + this.score);
+    console.log("Foul count is " + this.fouls);
+    return this;
+  },
+
+  fullTime() {
+    console.log("Fulltime score is " + this.score);
+    console.log("Foul count is " + this.fouls);
+    return this;
+  },
+reset() {
+  this.score = 0;    // set the score back to zero
+  this.fouls = 0;    // set the fouls back to zero
+  return this;
+},
+
+};
+
+basketballGame
+.reset()
+  .basket()
+  .foul()
+  .freeThrow()
+  .foul()
+  .basket()
+  .freeThrow()
+  .basket()
+  .threePointer()
+  .basket()
+  .foul()
+  .threePointer()
+  .halfTime()
+  .fullTime();
+
+====================Test 01====================
+basketballGame
+.reset()
+.basket()
+.basket()
+.threePointer()
+  .halfTime()
+  .fullTime();
+
+Output:  Halftime score is 7
+Foul count is 0
+Fulltime score is 7
+Foul count is 0
+
+====================Test 02====================
+basketballGame
+.reset()
+.foul()
+.foul()
+.foul()
+.freeThrow()
+  .halfTime()
+  .fullTime();
+
+Output: 
+Halftime score is 1
+Foul count is 3
+Fulltime score is 1
+Foul count is 3
+
+====================Test 03====================
+basketballGame
+.reset()
+.freeThrow()
+.basket()
+.foul()
+.threePointer()
+  .halfTime()
+  .fullTime();
+
+Output: 
+Halftime score is 6
+Foul count is 1
+Fulltime score is 6
+Foul count is 1
+========================================
 */
