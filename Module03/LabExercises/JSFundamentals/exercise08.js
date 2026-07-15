3b538df22fd943d4dde7f312a5a6e3271b735ac9
tabitha-codes <tabithacodes@gmail.com>
2026-07-14 19:03:56 -0500

    mod03ex08

--- Module03/LabExercises/JSFundamentals/exercise08.js
+++ Module03/LabExercises/JSFundamentals/exercise08.js
@@ -16,15 +16,91 @@ const sydney = {
   timezone: "Australia/Sydney",
 };
 
-/* 
-My Answer:
-Output:
+const memphis = {
+  name: "Memphis",
+  population: 610_000,
+  state: "USA",
+  founded: "22 May 1819",
+  timezone: "Central Standard Time",
+};
 
-Notes:
+function printCity(city) { 
+for (let key in city) { // iterates over each property in the phone object, stores in ‘key’ variable
+console.log('key: ' + key); // prints each object property name (key) in turn
+console.log('value: ' + city[key]); // prints each object value in turn
+}
+}
+printCity(sydney);
+printCity(memphis);
 
+/* My Answer:
 a)
+const sydney = {
+  name: "Sydney",
+  population: 5_121_000,
+  state: "NSW",
+  founded: "26 January 1788",
+  timezone: "Australia/Sydney",
+};
 
+function printCity(city) { 
+for (let key in city) { // iterates over each property in the phone object, stores in ‘key’ variable
+console.log('key: ' + key); // prints each object property name (key) in turn
+console.log('value: ' + city[key]); // prints each object value in turn
+}
+}
+printCity(sydney);
+
+Output: 
+key: state
+value: NSW
+key: founded
+value: 26 January 1788
+key: timezone
+value: Australia/Sydney
+==================================================
 b) 
 
+const sydney = {
+  name: "Sydney",
+  population: 5_121_000,
+  state: "NSW",
+  founded: "26 January 1788",
+  timezone: "Australia/Sydney",
+};
 
+const memphis = {
+  name: "Memphis",
+  population: 610_000,
+  state: "USA",
+  founded: "22 May 1819",
+  timezone: "Central Standard Time",
+};
+
+function printCity(city) { 
+for (let key in city) { // iterates over each property in the phone object, stores in ‘key’ variable
+console.log('key: ' + key); // prints each object property name (key) in turn
+console.log('value: ' + city[key]); // prints each object value in turn
+}
+}
+printCity(sydney);
+printCity(memphis);
+
+Output: 
+key: state
+value: NSW
+key: founded
+value: 26 January 1788
+key: timezone
+value: Australia/Sydney
+key: name
+value: Memphis
+key: population
+value: 610000
+key: state
+value: USA
+key: founded
+value: 22 May 1819
+key: timezone
+value: Central Standard Time
 */
