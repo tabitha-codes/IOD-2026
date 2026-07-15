514774289dc8bf97dfdfcad75f2c045774623510
tabitha-codes <tabithacodes@gmail.com>
2026-07-14 21:53:42 -0500

    mod03ex09

--- Module03/LabExercises/JSFundamentals/exercise09.js
+++ Module03/LabExercises/JSFundamentals/exercise09.js
@@ -12,10 +12,51 @@ changed? Why?
 
 e) Change the way the moreSports and cat2 variables are created to ensure the
 originals remain independent
-
 */
-
 
 let teamSports = ['Hockey', 'Cricket', 'Volleyball'];
 let dog1 = 'Bingo';
 let cat1 = { name: 'Fluffy', breed: 'Siberian' };
+ // console.log(teamSports, dog1, cat1);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* My Answers:
+
+A] let moreSports = teamSports;
+teamSports.push("Soccer");
+moreSports.unshift("Football");
+console.log(teamSports);
+Output: [ 'Football', 'Hockey', 'Cricket', 'Volleyball', 'Soccer' ]
+============================
+B] let dog2 = dog1;
+dog2 = 'Bear';
+console.log(dog1); // Bingo
+console.log(dog2); // Bear
+Output: Bingo -  Bear
+============================
+C] let cat2 = cat1;
+cat2.name = 'Gary';
+console.log(cat1); // name: 'Fluffy'
+console.log(cat2); // name: 'Gary'
+Output: 'Fluffy' ,  'Gary'
+============================
+D] Yes, they have. 
+dog1 = 'Bingo' is a string
+teamSports = ['Hockey', ...] is an array
+cat1 = { name: 'Fluffy' } is an object
+============================
+E]
+Output:
+*/
