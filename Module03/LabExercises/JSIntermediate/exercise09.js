4e988963952f0669e20c3ae8d7bb90a5fcb9761c
tabitha-codes <tabithacodes@gmail.com>
2026-07-28 14:55:48 -0500

    Mod030209

--- Module03/LabExercises/JSIntermediate/exercise09.js
+++ Module03/LabExercises/JSIntermediate/exercise09.js
@@ -13,38 +13,65 @@ salaries
 b) Write a function topEarner(salaries) that calculates and returns the name of the
 person earning the highest salary*/
 
+let salaries = {
+"Timothy" : 35000,
+"David" : 25000,
+"Mary" : 55000,
+"Christina" : 75000,
+"James" : 43000
+};
 
 
+/* MY ANSWERS:
 
+A] let salaries = {
+"Timothy" : 35000,
+"David" : 25000,
+"Mary" : 55000,
+"Christina" : 75000,
+"James" : 43000
+};
 
+function sumSalaries(salaries){
+   let total = 0;
+  for (let key in salaries) {
+    total += salaries[key];
+  }
+  return total;
+}
 
+console.log(sumSalaries(salaries))
 
-/*
-
-MY ANSWERS:
-
-A]
-
-OUTPUT:
+OUTPUT: 233000
 =====================================
-B]
+B]function topEarner(salaries) {
+  const entries = Object.entries(salaries);
+  if (entries.length === 0) return null;
 
-OUTPUT:
-=====================================
-C]
+  return entries.reduce((max, current) => {
+    return current[1] > max[1] ? current : max;
+  })[0];
+}
 
-OUTPUT:
-=====================================
-D]
+let salaries = {
+"Timothy" : 35000,
+"David" : 25000,
+"Mary" : 55000,
+"Christina" : 75000,
+"James" : 43000
+};
 
-OUTPUT:
-=====================================
-E]
+function sumSalaries(salaries){
+   let total = 0;
+  for (let key in salaries) {
+    total += salaries[key];
+  }
+  return total;
+}
 
-OUTPUT:
-=====================================
-F]
+// console.log(sumSalaries(salaries))
+console.log(topEarner(salaries)); 
 
-OUTPUT:
+OUTPUT:  Christina
 =====================================
 */
