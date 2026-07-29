7213616e522893faf64c7903b398c192074c8894
tabitha-codes <tabithacodes@gmail.com>
2026-07-28 19:20:38 -0500

    Mod030301

---
+++ Module03/LabExercises/JSAdvanced/exercise01.js
@@ -0,0 +1,107 @@
+/* 1. makeCounter below is a decorator function which creates and returns a function that
+increments a counter.
+a) Create a second counter counter2 using the makeCounter function and test to see if
+it remains independent to counter1
+b) Modify makeCounter so that it takes an argument startFrom specifying where the
+counter starts from (instead of always starting from 0)
+c) Modify makeCounter to take another argument incrementBy, which specifies how
+much each call to counter() should increase the counter value by.*/
+
+
+
+function makeCounter(startFrom, incrementBy) {
+let currentCount = startFrom;
+
+return function() {
+console.log(currentCount)
+currentCount += incrementBy;
+return currentCount;
+};
+}
+// let counter1 = makeCounter();
+let counter1 = makeCounter(5, 10);
+let counter2 = makeCounter(3, 3);
+
+counter1();
+counter2();
+counter1();
+counter2();
+
+
+
+
+
+
+
+
+
+
+/* MY ANSWERS:
+
+A] function makeCounter() {
+let currentCount = 0;
+return function() {
+currentCount++;
+console.log(currentCount)
+return currentCount;
+};
+}
+let counter1 = makeCounter();
+let counter2 = makeCounter();
+counter1(); // 1
+counter1(); // 2
+counter2(); // 1
+counter2(); // 2
+
+
+Output: 1
+2
+1
+2
+=========================
+B] function makeCounter(startFrom) {
+let currentCount = startFrom;
+
+
+return function() {
+console.log(currentCount)
+currentCount++;
+return currentCount;
+};
+}
+
+let counter1 = makeCounter(5);
+let counter2 = makeCounter(100);
+
+counter1(); // 1
+counter1(); // 2
+counter2(); // 1
+counter2(); // 2
+
+
+Output: 5
+6
+100
+101
+=========================
+C]
+
+
+Output:
+=========================
+D]
+
+
+Output:
+=========================
+E]
+
+
+Output:
+=========================
+F]
+
+
+Output:
+=========================
+*/
