78731516204410ceb643b4c5c4931e0f2075977e
tabitha-codes <tabithacodes@gmail.com>
2026-08-02 16:28:48 -0500

    Mod030306

--- Module03/LabExercises/JSAdvanced/exercise06.js
+++ Module03/LabExercises/JSAdvanced/exercise06.js
@@ -1,53 +1,71 @@
+/*6. Use the Function prototype to add a new delay(ms) function to all functions, which can
+be used to delay the call to that function by ms milliseconds.
+function multiply(a, b) {
+console.log( a * b );
+}
+multiply.delay(500)(5, 5); // prints 25 after 500 milliseconds
+a) Use the example multiply function below to test it with, as above, and assume that all
+delayed functions will take two parameters
+b) Use apply to improve your solution so that delayed functions can take any number of
+parameters
+c) Modify multiply to take 4 parameters and multiply all of them, and test that your
+delay prototype function still works. */
 
 
 
+function multiply(a, b) {
+   
+console.log( a * b );
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+multiply.delay(500)(5, 5); // prints 25 after 500 milliseconds
+                           
 
 
 /* MY ANSWERS:
+A] Function.prototype.delay = function(ms) {
+  let originalFn = this;        
+  return function(a, b) {       
+    setTimeout(function() {
+      originalFn(a, b);         
+    }, ms);
+  };
+};
 
-A] 
+function multiply(a, b) { console.log(a * b); }
+multiply.delay(500)(5, 5);      // prints 25 after 500ms
 
 
-Output:
+Output:25
 =========================
-B]
+B] Function.prototype.delay = function(ms) {
+  let originalFn = this;
+  return function(...args) {          
+    setTimeout(function() {
+      originalFn.apply(this, args);   
+    }, ms);
+  };
+};
+
+function multiply(a, b) { console.log(a * b); }
+multiply.delay(500)(5, 5);            // still prints 25 after 500ms
 
 
-Output:
+Output: 25
 =========================
-C]
+C] Function.prototype.delay = function(ms) {
+  let originalFn = this;
+  return function(...args) {
+    setTimeout(function() {
+      originalFn.apply(this, args);
+    }, ms);
+  };
+};
+
+function multiply(a, b, c, d) { console.log(a * b * c * d); }   
+multiply.delay(500)(2, 3, 4, 5);  
 
 
-Output:
-=========================
-D]
-
-
-Output:
-=========================
-E]
-
-
-Output:
-=========================
-F]
-
-
-Output:
+Output:120
 =========================
 */
