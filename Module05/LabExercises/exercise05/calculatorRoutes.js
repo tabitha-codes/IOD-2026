8c3a454341829457e30882fb7d5ab49e45ab217b
tabitha-codes <tabithacodes@gmail.com>
2026-08-23 17:05:17 -0500

    Mod0505 controllers

--- Module05/LabExercises/exercise05/calculatorRoutes.js
+++ Module05/LabExercises/exercise05/calculatorRoutes.js
@@ -1,46 +1,63 @@
 const express = require('express');
 const router = express.Router();
+const calculatorController = require('./calculatorController');
+
+router.get('/add', (req, res) => {
+    calculatorController.addNumbers(req, res)
+})
+
+router.get('/subtract', (req, res) => {
+    calculatorController.subtractNumbers(req, res)
+})
+
+router.get('/multiply', (req, res) => {
+    calculatorController.multiplyNumbers(req, res)
+})
+
+router.get('/divide', (req, res) => {
+    calculatorController.divideNumbers(req, res)
+})
+
 
 
 // functional add route performing addition on request parameters
-router.get('/add', (req, res) => {
-let number1 = parseInt(req.query.num1);
-let number2 = parseInt(req.query.num2);
-let sum = number1 + number2
-console.log(sum)
-res.status(200)
-res.json({result:sum})
-})
+// router.get('/add', (req, res) => {
+// let number1 = parseInt(req.query.num1);
+// let number2 = parseInt(req.query.num2);
+// let sum = number1 + number2
+// console.log(sum)
+// res.status(200)
+// res.json({result:sum})
+// })
 
 // functional subtraction route performing subtraction on request parameters
-router.get('/subtract', (req, res) => {
-let number1 = parseInt(req.query.num1);
-let number2 = parseInt(req.query.num2);
-let subtract = number1 - number2
-console.log(subtract)
-res.status(200)
-res.json({result:subtract})
-})
+// router.get('/subtract', (req, res) => {
+// let number1 = parseInt(req.query.num1);
+// let number2 = parseInt(req.query.num2);
+// let difference = number1 - number2
+// console.log(difference)
+// res.status(200)
+// res.json({result:difference})
+// })
 
 // functional multiply route performing multiplication on request parameters
-router.get('/multiply', (req, res) => {
-let number1 = parseInt(req.query.num1);
-let number2 = parseInt(req.query.num2);
-let multiply = number1 * number2
-console.log(multiply)
-res.status(200)
-res.json({result:multiply})
-})
+// router.get('/multiply', (req, res) => {
+// let number1 = parseInt(req.query.num1);
+// let number2 = parseInt(req.query.num2);
+// let product = number1 * number2
+// console.log(product)
+// res.status(200)
+// res.json({result:product})
+// })
 
 // functional division route performing division on request parameters
-router.get('/divide', (req, res) => {
-let number1 = parseInt(req.query.num1);
-let number2 = parseInt(req.query.num2);
-let divide = number1 / number2
-console.log(divide)
-res.status(200)
-res.json({result:divide})
-})
-
+// router.get('/divide', (req, res) => {
+// let number1 = parseInt(req.query.num1);
+// let number2 = parseInt(req.query.num2);
+// let quotient = number1 / number2
+// console.log(quotient)
+// res.status(200)
+// res.json({result:quotient})
+// })
 
 module.exports = router;
